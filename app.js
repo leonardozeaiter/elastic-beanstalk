a85f901e63a6b1e429dbb64ec0d6833e98dbd6d6
Leonardo Zeaiter <leonardozeaiter@gmail.com>
2021-04-25 12:16:44 +0300

    NB2

--- app.js
+++ app.js
@@ -8,9 +8,23 @@ const Note = require('./Note')
 
 app.get('/', async (req, res) => {
     await connectToDatabase()
-    const notes = await Note.find()
+
+    await Note.insertMany([{ title: "t", description: "d" }], (err, data) => {
+        console.log("ERR: ", err);
+        console.log("DATA: ", data)
+    })
+
+    const notes = await Note.find({}, (err, data) => {
+        //console.log("ERR: ", err);
+        //console.log("DATA: ", data)
+    });
+
+
     res.send({ v: 1, notes })
 })
 
 //Test
-http.createServer(app).listen(process.env.PORT || 8000)
+http.createServer(app).listen(process.env.PORT || 8000, () => {
+    console.log(process.env.DB)
+    console.log("APP IS listening...");
+})
--- db.js
+++ db.js
@@ -3,11 +3,14 @@ mongoose.Promise = global.Promise
 let isConnected
 
 module.exports = async = () => {
+
+    console.log("Is Connected: ", isConnected)
     if (isConnected) return Promise.resolve()
 
-    return mongoose.connect(process.env.DB)
+    return mongoose.connect(process.env.DB, { useUnifiedTopology: true, useNewUrlParser: true })
         .then(db => {
             isConnected = db.connections[0].readyState
+            console.log(db.connections[0])
         })
 
 }
