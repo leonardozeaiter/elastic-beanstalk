2c27f98b1bc1d92d1a09d4c7b58715db25e188ab
Leonardo Zeaiter <leonardozeaiter@gmail.com>
2021-04-29 20:30:45 +0300

    NB34

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.get('/', async (req, res) => {
 })
 
 //Test
-http.createServer(app).listen(process.env.PORT || 3000, () => {
+http.createServer(app).listen(process.env.PORT || 80, () => {
     //console.log(process.env.DB)
     console.log("APP IS listening...");
 })
