4988d845fd6e3d231d2c6328539939ddfc3d7e2f
Leonardo Zeaiter <leonardozeaiter@gmail.com>
2021-04-29 22:39:26 +0300

    NB42

--- db.js
+++ db.js
@@ -7,7 +7,10 @@ module.exports = async = () => {
     console.log("Is Connected: ", isConnected)
     if (isConnected) return Promise.resolve()
 
-    return mongoose.connect(process.env.DB, { useUnifiedTopology: true, useNewUrlParser: true })
+    let conUrl = "mongodb+srv://mflixAppUser:mflixAppPwd@mflix.a5soe.mongodb.net/sample_mflix";
+
+    // return mongoose.connect(process.env.DB, { useUnifiedTopology: true, useNewUrlParser: true })
+    return mongoose.connect(conUrl, { useUnifiedTopology: true, useNewUrlParser: true })
         .then(db => {
             isConnected = db.connections[0].readyState
             //console.log(db.connections[0])
